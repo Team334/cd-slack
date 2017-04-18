74d5aab8889f3c14c1e81599ce79696dbd2a2eff
August <augustt198@gmail.com>
2017-04-18 15:16:18 -0400

    Update example config & README

--- config.example.js
+++ config.example.js
@@ -1,20 +1,30 @@
-var config = {
+module.exports = {
     "slack_webhook_url": "<your incoming webhook url>",
     "bot_username": "Chief Delphi Bot", // whatever you want
     "bot_icon_emoji": ":first:",        // the emoji to use as avatar
-    "channel": "#programming",          // the channel to post in
+    "default_channel": "#programming",  // the channel to post in (by default)
 
     // how long to wait between updates in milliseconds
     "refresh_rate": 15*1000, // 15 seconds
 
-    // Which keywords trigger the bot. Can use regex.
-    "keywords": [
-        "334", "bamboozled", "opencv",
-        /gracious (professionalism|memes)/gi
+    // what keywords/authors trigger the bot, where to post notification,
+    // and who to notify
+    "triggers": [
+        {
+            "keywords": ["New York City", "NYC", "brooklyn"],
+            "channel": "#general"
+        },
+        {
+            "keywords": ["computer vision", "opencv", "neural networks"],
+            "authors": ["Tom Bottiglieri", "Jared Russell"],
+            "channel": "#programming",
+            "mention": "@august @eric"
+        },
+        {
+            "keywords": ["bamboozled", /gracious (professionalism|memes)/gi],
+            "channel": "#memes"
+        }
     ],
-
-    // Which authors trigger the bot (for interesting people)
-    "authors": ["augustt198"]
 
     // How the messages should be formatted. These special symbols will be substituted:
     // $keyword - the keyword(s) that triggered the bot
@@ -26,5 +36,3 @@ var config = {
     "format_keyword": "New mention of *$keyword* by $author: <$url|$post>",
     "format_author": "New post by *$author*: <$url|$post>"
 }
-
-module.exports = config;
